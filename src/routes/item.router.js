74373c6069a05df94c9ea72587fc8c6bb563b134
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-09 17:23:03 +0900

    feat: "아이템 목록 조회 API 추가"
    생성된 아이템 목록을 조회하는 API 추가 item_code가 높은 순으로 정렬

--- src/routes/item.router.js
+++ src/routes/item.router.js
@@ -13,7 +13,6 @@ router.post('/items', async (req, res, next) => {
   try {
     // 유효성 검사
     const schema = Joi.object({
-      item_code: Joi.number().integer().required(), // 정수만 입력 가능
       item_name: Joi.string()
         .pattern(/^[가-힣\s]+$/) // 한글과 공백만 입력 가능
         .min(2)
@@ -32,7 +31,6 @@ router.post('/items', async (req, res, next) => {
 
     const item = await prisma.items.create({
       data: {
-        item_code: +item_code,
         item_name,
         item_stat,
         item_price: +item_price,
@@ -72,6 +70,29 @@ router.patch('/items/:item_code', async (req, res, next) => {
     });
 
     return res.status(200).json({ data: updatedItem });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * 아이템 목록 조회 API
+ */
+router.get('/items', async (req, res, next) => {
+  try {
+    // 생성된 아이템 목록 조회
+    const items = await prisma.items.findMany({
+      select: {
+        item_code: true,
+        item_name: true,
+        item_price: true,
+      },
+      orderBy: {
+        item_code: 'desc', // 코드가 높은 순으로 정렬
+      },
+    });
+
+    return res.status(200).json({ data: items });
   } catch (error) {
     next(error);
   }
